12f2c4f1820edc5a4b1f38b1bea581c17bc41e46
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-05-14 02:09:55 +0100

    Adds second test & supporting stub class.

---
+++ Lucky/Exceptions.py
@@ -0,0 +1,8 @@
+'''
+Created on 14 May 2016
+
+@author: wnm24546
+'''
+
+class LuckyConfigException(Exception):
+    pass
--- tests/test_integrationConfig.py
+++ tests/test_integrationConfig.py
@@ -4,8 +4,10 @@ Created on 14 May 2016
 @author: wnm24546
 '''
 from Lucky.IntegrationConfig import IntegrationConfig
+from Lucky.Exceptions import LuckyConfigException
 
 from nose.tools import assert_true
+from nose.tools import assert_raises
 
 class TestIntegrationConfig(object):
     
@@ -18,3 +20,10 @@ class TestIntegrationConfig(object):
         self.iConf.window = 300
         
         assert_true(self.iConf.checkConfig())
+
+    def test_end_lessthan_start(self):
+        self.iConf.start = 900
+        self.iConf.end = 200
+        self.iConf.window = 300
+        
+        assert_raises(LuckyConfigException, self.iConf.checkConfig)
